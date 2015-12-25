052dcb2a7fb5891b3f6d08405d4f239aad781ac7
Ruben Vermeulen <ruben.vermeulen@hotmail.com>
2015-12-25 18:00:01 +0100

    Added published at to create Article

--- app/routes/article/create.php
+++ app/routes/article/create.php
@@ -2,7 +2,10 @@
 
 $app->get('/articles/create', $authenticated, function() use ($app) {
 
-    $app->render('article/create.twig');
+    $app->render('article/create.twig', [
+        'date' => date('Y-m-d'),
+        'time' => date('H:i'),
+    ]);
 
 })->name('article.create');
 
@@ -15,18 +18,23 @@ $app->post('/articles/create', $authenticated, function() use ($app) {
 
     $subject = $request->post('subject');
     $content = $request->post('content');
+    $date = $request->post('date');
+    $time = $request->post('time');
 
     $v = $app->validation;
 
     $v->validate([
         'subject|Onderwerp' => [$subject, 'required|max(150)'],
         'content|Bericht' => [$content, 'required|max(30000)'],
+        'date|Datum' => [$date, 'required|date'],
+        'time|Tijd' => [$time, 'required'],
     ]);
 
     if ($v->passes()) {
         $app->article->create([
             'subject' => $subject,
             'content' => $content,
+            'published_at' => \Carbon\Carbon::createFromTimestamp(strtotime($date . ' '.  $time)),
         ]);
 
         $app->flash('global', 'Het artikel is aangemaakt.');
