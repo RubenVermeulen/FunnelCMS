f4f8dba95265585559c1945a38e3bd5e90b827e0
Ruben Vermeulen <ruben.vermeulen@hotmail.com>
2016-08-08 12:17:14 +0200

    Removes pagination for pages

--- app/routes/page/all.php
+++ app/routes/page/all.php
@@ -1,29 +1,13 @@
 <?php
 
-use \FunnelCms\Helpers\Pagination;
+$app->get('/pages', $authenticated(), function() use($app) {
 
-function pagesAllRoute($app, $page) {
     $items = $app->page
-        ->oldest('priority');
-
-    $pagination = new Pagination($app, $items, $page);
-    $pagination->execute();
+        ->oldest('priority')
+        ->get();
 
     $app->render('page/all.twig', [
-        'items' => $pagination->getResult(),
-        'pages' => $pagination->getTotalPages(),
-        'page' => $page,
+        'items' => $items,
     ]);
-};
-
-$app->get('/pages', $authenticated(), function($page = 1) use($app) {
-
-    pagesAllRoute($app, $page);
 
 })->name('page.all');
-
-$app->get('/pages/page/:page', $authenticated(), function($page = 1) use($app) {
-
-    pagesAllRoute($app, $page);
-
-})->name('page.all.page');
