f2a1749104f2516dc4671d40026d59508cf53f88
Ruben Vermeulen <ruben.vermeulen@hotmail.com>
2016-08-25 22:16:49 +0200

    Adds migration for articles table

---
+++ app/database/migrations/20160825200403_create_articles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateArticlesTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('articles');
+
+        $table
+            ->addColumn('user_id', 'integer')
+            ->addForeignKey('user_id', 'users', 'id', ['delete' => 'NO_ACTION', 'update' => 'NO_ACTION'])
+            ->addColumn('subject', 'string', ['limit' => 150])
+            ->addColumn('summary', 'string', ['limit' => 255])
+            ->addColumn('content', 'text')
+            ->addTimestamps()
+            ->addColumn('published_at', 'timestamp')
+            ->save();
+    }
+}
