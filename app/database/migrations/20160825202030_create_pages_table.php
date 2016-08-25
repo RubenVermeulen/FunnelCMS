95f09178fdb8ff970c523816d61d81b74fabaa85
Ruben Vermeulen <ruben.vermeulen@hotmail.com>
2016-08-25 22:22:12 +0200

    Adds migration for pages table

---
+++ app/database/migrations/20160825202030_create_pages_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreatePagesTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('pages');
+
+        $table
+            ->addColumn('name', 'string', ['limit' => 50])
+            ->addColumn('slug', 'string', ['limit' => 100])
+            ->addColumn('content', 'text')
+            ->addColumn('is_visible', 'boolean')
+            ->addColumn('is_locked', 'boolean')
+            ->addColumn('priority', 'integer')
+            ->addTimestamps()
+            ->save();
+    }
+}
