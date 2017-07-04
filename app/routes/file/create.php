c27f3cba6dfb25ad072aceff2aff9d665074691a
Ruben Vermeulen <ruben.vermeulen@hotmail.com>
2017-07-04 19:24:47 +0200

    Adds multi file upload (WIP)

--- app/routes/file/create.php
+++ app/routes/file/create.php
@@ -21,37 +21,40 @@ $app->post('/files/create', $authenticated(), function() use($app) {
     {
         try
         {
-            $allowedExtensions = ['jpeg', 'png', 'gif', 'pdf'];
+            $allowedExtensions = $app->config->get('storage.rules')['extensions'];
+            $maxSize = $app->config->get('storage.rules')['maxSize'];
             $request = Request::createFromGlobals();
-            $file = $request->files->get('file');
+            $files = $request->files->get('file');
 
-            if( ! $file)
-                throw new \Exception('Please choose a file');
+            foreach ($files as $file) {
+                if( ! $file)
+                    throw new \Exception('Please choose a file');
 
-            if ($file->getError() != UPLOAD_ERR_OK)
-                throw new \Exception($app->translator->get('CouldNotUploadFile'));
+                if ($file->getError() != UPLOAD_ERR_OK)
+                    throw new \Exception($app->translator->get('CouldNotUploadFile'));
 
-            if ( ! in_array(strtolower($file->guessClientExtension()), $allowedExtensions))
-                throw new \Exception($app->translator->get('ExtensionNotAllowedFiles'));
+                if ( ! in_array(strtolower($file->guessClientExtension()), $allowedExtensions))
+                    throw new \Exception($app->translator->get('ExtensionNotAllowedFiles'));
 
-            if ($file->getClientSize() > 500000)
-                throw new \Exception(sprintf($app->translator->get('SizeLimitExceeded'), 500000 / 1000000));
+                if ($file->getClientSize() > $maxSize)
+                    throw new \Exception(sprintf($app->translator->get('SizeLimitExceeded'), $maxSize / 1000000));
 
-            $uploadedFile = new UploadedFile($file, $app->storageProvider);
+                $uploadedFile = new UploadedFile($file, $app->storageProvider);
 
-            $items = explode('/', $uploadedFile->store('files'));
+                $items = explode('/', $uploadedFile->store('files'));
 
-            $name = array_pop($items);
-            $path = empty($items) ? null : implode('/', $items);
+                $name = array_pop($items);
+                $path = empty($items) ? null : implode('/', $items);
 
-            $app->file->create([
-                'path' => $path,
-                'name_system' => $name,
-                'name_human' => strtolower(explode('.', $file->getClientOriginalname())[0]),
-                'size' => $file->getSize(),
-            ]);
+                $app->file->create([
+                    'path' => $path,
+                    'name_system' => $name,
+                    'name_human' => strtolower(explode('.', $file->getClientOriginalname())[0]),
+                    'size' => $file->getSize(),
+                ]);
+            }
 
-            $app->flash('global', sprintf($app->translator->get('FileUploaded'), $file->getClientOriginalname()));
+            $app->flash('global', 'uploaded');
             $app->redirect($app->urlFor('file.all'));
 
         } catch (\Exception $e)
